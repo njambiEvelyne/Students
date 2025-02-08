63fdaa9fde3b209a9b7c950b84c01aa6135309f4
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-08 12:23:01 +0300

    Added new pri=ojects

---
+++ Student Class/Quiz Game.py
@@ -0,0 +1,13 @@
+import random
+
+questions ={
+1: "What is your name?",
+2: "What is your favourite language?",
+3: "How many siblings do you have?",
+4: "What is your course\Career path?",
+5: "What challenges are you facing in your learning?"
+
+}
+
+choice = random.choice
+selection = choice(input("Enter your choice: "))
