5b18979dd45e1fbfe1d5a01ecde8bd45f671f208
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-08 13:41:42 +0300

    Added a guessing game project

--- Student Class/NumberGuess.py
+++ Student Class/NumberGuess.py
@@ -1,16 +1,17 @@
 import random
+import emoji
 
-secrect_number = random.randint(0, 10)
+secrect_number = random.randint(0, 3)
 
 try:
     while True:
         guess = int(input("Enter the secret number: "))
-        if guess == secrect_number: 
-            print("You won!")
+        if guess == secrect_number:
+            print(emoji.emojize("You won! :party_popper:"))  # 🎉             
             break
         
         else:
-            print("Sorry. Try again!")
+            print(emoji.emojize("Sorry. Try again! :crying_face:"))
         play_again = input("Do you want to play again(yes, no): ")
         if play_again != "yes":
             print("Thanks for playing. Goodbye")
